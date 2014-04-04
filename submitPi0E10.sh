01ae2aa4c579fbdd503ea56984f0bec81372a234
Laura Dodd <lmdodd@wisc.edu>
2014-04-04 13:28:53 -0500

    submit single file

---
+++ submitPi0E10.sh
@@ -0,0 +1,1 @@
+cmsDriver.py SinglePi0E10_cfi.py --fileout file:pi0E10.root --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1,Configuration/DataProcessing/Utils.addMonitoring --conditions POSTLS162_V2::All --step GEN,SIM,DIGI,L1,DIGI2RAW,RAW2DIGI,L1Reco,RECO --eventcontent=FEVTSIM --magField 38T_PostLS1 --geometry Extended2015 -n 1000
